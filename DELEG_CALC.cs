65fa64916442ef743170fb01760fbd5aa6d15f23
Mohammed Ahmed Amin <snake12isawesome@gmail.com>
2025-04-27 02:15:31 +0300

    Add files via upload

---
+++ DELEG_CALC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DELEGATE_CALC
+{
+     internal class Program
+    {
+        delegate int Calc(int a, int b);
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main()
+        {
+            Calc add = Add;
+            Calc subtract = Sub;
+            Calc multiply = Mul;
+            int num1 = 10, num2 = 5;
+            Console.WriteLine($"Add: {add(num1, num2)}");
+            Console.WriteLine($"Subtract: {subtract(num1, num2)}");
+            Console.WriteLine($"Multiply: {multiply(num1, num2)}");
+        }
+    }
+
+}

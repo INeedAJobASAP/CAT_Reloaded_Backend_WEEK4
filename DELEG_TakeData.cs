0f9faaefd9ccc90741e979e170c0a16527131810
Mohammed Ahmed Amin <snake12isawesome@gmail.com>
2025-04-27 02:16:01 +0300

    Add files via upload

---
+++ DELEG_TakeData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DELEG_TakeData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
